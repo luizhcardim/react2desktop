6fa20df48ae5bbe321d86f0399132c7c1b50335a
Luiz Henrique A Cardim <luizhcardim@gmail.com>
2020-10-06 09:07:14 -0300

    Small ajustments

--- README.md
+++ README.md
@@ -13,16 +13,71 @@ npm install --save react2desktop
 ## Usage
 
 ```jsx
-import React, { Component } from 'react'
+import React from 'react'
 
-import MyComponent from 'react2desktop'
+import Desktop from 'react2desktop'
 import 'react2desktop/dist/index.css'
+import { BlankPage } from './blank'
 
-class Example extends Component {
-  render() {
-    return <MyComponent />
-  }
+const App = () => {
+
+  // Passing the menu options
+  let menu = [
+    {
+
+      name: 'Module Name 1', // Name of the first module
+      icon: 'ri-focus-line',  // Icon of the first module
+      items: [
+        {
+          name: 'Menu 1', items: [ // Name of the first menu of the first module
+            { name: 'Option 1', icon: 'center.png', component: <BlankPage></BlankPage> }, // The options from the menu...
+            { name: 'Option 2', icon: 'ri-building-line', component: <BlankPage></BlankPage> },
+          ]
+        },
+        {
+          name: 'Menu 2', items: [ // Name of the first menu of the first module
+            { name: 'Option 1', icon: 'center.png', component: <BlankPage></BlankPage> }, // The options from the menu...
+            { name: 'Option 2', icon: 'ri-building-line', component: <BlankPage></BlankPage> },
+            { name: 'Option 3', icon: 'ri-building-line', component: <BlankPage></BlankPage> },
+          ]
+        },
+      ]
+    },
+    {
+
+      name: 'Module Name 2', // Name of the second module
+      icon: 'ri-book-open-fill', // Icon of the second module
+      items: [
+        {
+          name: 'Menu 1', items: [
+            { name: 'Option 1', icon: 'center.png', component: <BlankPage></BlankPage> },
+            { name: 'Option 2', icon: 'center.png', component: <BlankPage></BlankPage> }
+          ]
+        },
+        { name: 'Direct Option', icon: 'center.png', component: <BlankPage></BlankPage> }
+      ]
+    }
+  ]
+
+  // Options from the user menu
+  let user_menu = [
+    {
+      name: 'My Profile',
+      component: <BlankPage></BlankPage>
+    },
+    {
+      name: 'Logout',
+      action: () => console.log('Logout...')
+    },
+
+  ]
+
+  
+
+  return <Desktop menu_options={menu} user_menu={user_menu} />
 }
+
+export default App
 ```
 
 ## License
--- example/src/App.js
+++ example/src/App.js
@@ -10,34 +10,36 @@ const App = () => {
   let menu = [
     {
 
-      name: 'Module 1', // Name of the first module
+      name: 'Module Name 1', // Name of the first module
       icon: 'ri-focus-line',  // Icon of the first module
       items: [
         {
-          name: 'First Menu Name', items: [ // Name of the first menu of the first module
-            { name: 'Bairros', icon: 'center.png', component: <BlankPage></BlankPage> }, // The options from the menu...
-            { name: 'Cargos', icon: 'ri-building-line', component: <BlankPage></BlankPage> },
-            { name: 'Cidades', icon: 'ri-building-line', component: <BlankPage></BlankPage> },
-            { name: 'Funcionários', icon: 'ri-building-line', component: <BlankPage></BlankPage> },
-            { name: 'Pessoas', icon: 'ri-building-line', component: <BlankPage></BlankPage> },
-            { name: 'Unidades Organizacionais', icon: 'ri-building-line', component: <BlankPage></BlankPage> },
-
+          name: 'Menu 1', items: [ // Name of the first menu of the first module
+            { name: 'Option 1', icon: 'center.png', component: <BlankPage></BlankPage> }, // The options from the menu...
+            { name: 'Option 2', icon: 'ri-building-line', component: <BlankPage></BlankPage> },
+          ]
+        },
+        {
+          name: 'Menu 2', items: [ // Name of the first menu of the first module
+            { name: 'Option 1', icon: 'center.png', component: <BlankPage></BlankPage> }, // The options from the menu...
+            { name: 'Option 2', icon: 'ri-building-line', component: <BlankPage></BlankPage> },
+            { name: 'Option 3', icon: 'ri-building-line', component: <BlankPage></BlankPage> },
           ]
         },
       ]
     },
     {
 
-      name: 'Module 2', // Name of the second module
-      icon: 'ri-book-open-fill',
+      name: 'Module Name 2', // Name of the second module
+      icon: 'ri-book-open-fill', // Icon of the second module
       items: [
         {
-          name: 'Cadastros', items: [
-            { name: 'Cidade 3', icon: 'center.png', component: <BlankPage></BlankPage> },
-            { name: 'Bairro 3', icon: 'center.png', component: <BlankPage></BlankPage> }
+          name: 'Menu 1', items: [
+            { name: 'Option 1', icon: 'center.png', component: <BlankPage></BlankPage> },
+            { name: 'Option 2', icon: 'center.png', component: <BlankPage></BlankPage> }
           ]
         },
-        { name: 'Cidade 2', icon: 'center.png', component: <BlankPage></BlankPage> }
+        { name: 'Direct Option', icon: 'center.png', component: <BlankPage></BlankPage> }
       ]
     }
   ]
@@ -45,15 +47,17 @@ const App = () => {
   // Options from the user menu
   let user_menu = [
     {
-      name: 'Meus dados',
+      name: 'My Profile',
       component: <BlankPage></BlankPage>
     },
     {
-      name: 'Sair',
-      action: () => console.log('SAIR')
+      name: 'Logout',
+      action: () => console.log('Logout...')
     },
 
   ]
+
+  
 
   return <Desktop menu_options={menu} user_menu={user_menu} />
 }
